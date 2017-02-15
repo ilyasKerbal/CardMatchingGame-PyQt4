d91e1eaba742db10d1498f4e975191bad3d9c478
ILYAS KERBAL <kerbalsc@gmail.com>
2017-02-15 22:55:29 +0000

    Set an Application User Model ID for Windows

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+try:
+    import ctypes
+    myappid = 'kerbalapp.cardmatching.github.one'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+except Exception:
+    pass
+
 if __name__ == "__main__":
     import sys
     from package import app
